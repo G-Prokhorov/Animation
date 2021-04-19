1666062915e8761eeae3629558f583922dab4032
Gregory Prokhorov <grisha.prokhorov03@gmail.com>
2021-04-19 15:18:53 +0300

    antispam

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ let notify = document.getElementById("notification");
 let logotg = document.getElementById("logotg");
 let text = document.getElementById("text");
 
-
+let clicked = false;
 
 function show() {
-    notify.style.top = "30px";
-    setTimeout(() => {
-        logotg.style.left = "0";
-        text.style.width = "100%";
+    if (!clicked) {
+        clicked = true;
+        notify.style.top = "30px";
         setTimeout(() => {
-            logotg.style.left = "50%";
-            text.style.width = "0%";
+            logotg.style.left = "0";
+            text.style.width = "100%";
             setTimeout(() => {
-                notify.style.top = "-70px";
-            }, 850);
-        }, 5000);
-    }, 700);
+                logotg.style.left = "50%";
+                text.style.width = "0%";
+                setTimeout(() => {
+                    notify.style.top = "-70px";
+                    clicked = false;
+                }, 850);
+            }, 5000);
+        }, 700);
+    }
 }
 
 notify.addEventListener("click", () => {
